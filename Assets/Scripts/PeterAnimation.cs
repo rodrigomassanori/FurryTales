4078e94e1ef6a5111f9b63257b153fb656bbc8ab
TheVaxStudio <rodrigomassanori@hotmail.com>
2023-02-21 12:09:22 -0300

    saved

---
+++ Assets/Scripts/PeterAnimation.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class PeterAnimation : MonoBehaviour 
+{
+    float vel = 8.5f;
+
+    Rigidbody rb;
+
+    Animator an;
+
+    Vector3 mover;
+
+    bool AutoJump;
+
+    void Awake()
+    {
+        an = GetComponent<Animator>();
+
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
+
+        float v = Input.GetAxisRaw("Vertical") * Time.deltaTime;
+
+        Mover(h, v);
+
+        Animar(h, v);
+    }
+
+    void Mover(float h, float v)
+    {
+        mover.Set(h, 0.0f, v);
+
+        mover = mover.normalized * vel * Time.deltaTime;
+
+        rb.MovePosition(transform.position + mover);
+    }
+
+    void Animar(float h, float v)
+    {
+        bool walking = h != 0.0f || v != 0.0f;
+
+        an.SetBool("Walk", walking);
+    }
+}
