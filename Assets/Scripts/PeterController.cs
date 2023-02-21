811b1d4883812c35e87a359387a10c83c380dc65
TheVaxStudio <rodrigomassanori@hotmail.com>
2023-02-21 09:43:52 -0300

    saved

--- Assets/Scripts/PeterController.cs
+++ Assets/Scripts/PeterController.cs
@@ -1,6 +1,24 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PeterController : MonoBehaviour 
 {
+    float MoveSpeed = 2.0f;
+
+    float RotationSpeed = 4.0f;
+
+    float RunningSpeed;
+
+    float Vaxis, Haxis;
+    
+    public bool IsJumping, IsJumpingAlt, IsGrounded = false;
+    
+    Vector3 Movement;
+    
+    void Awake()
+    {
+        print("Initialized (" + this.name + ")");
+    }
+
     
 }
