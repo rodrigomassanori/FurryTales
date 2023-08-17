5e0309a5ad76d5f7a40d48f24c9f0aa349086f8c
TheVaxStudio <rodrigomassanori@hotmail.com>
2023-08-17 10:01:40 -0300

    saved all

--- Assets/Scripts/ElevatorMovement.cs
+++ Assets/Scripts/ElevatorMovement.cs
@@ -2,10 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//[RequireComponent(typeof(Rigidbody))]
 public class ElevatorMovement : MonoBehaviour
 {
-
-    // Coloque no inspector a quantidade de andares que voc� quiser
     [SerializeField]
     Transform[] levels;
 
@@ -19,20 +18,15 @@ public class ElevatorMovement : MonoBehaviour
 
     Rigidbody elevatorRb;
 
-    // Start is called before the first frame update
     void Awake()
     {
         elevatorRb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // Mudar andar desejado -> mude do jeito que quiser
-        // recomendo colocar um trigger dentro do elevador, e verificar se o jogador est� no trigger antes do jogador poder mudar o elevador
         if (Input.GetKeyDown(KeyCode.W))
         {
-            // IF especifico para evitar o erro (ArrayOutOfBounds)
             if (targetedLevel + 1 < levels.Length)
             {
                 targetedLevel++;
@@ -41,20 +35,17 @@ public class ElevatorMovement : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.S))
         {
-            // IF especifico para evitar o erro (ArrayOutOfBounds)
             if (targetedLevel - 1 >= 0)
             {
                 targetedLevel--;
             }
         }
 
-        // Verificar se o Elevador est� no andar desejado, se n�o tiver, o elevador ir� at� o andar
         if (transform.position.y != levels[targetedLevel].transform.position.y)
         {
             GoTo(levels[targetedLevel], elevatorRb);
         }
 
-        // Fun��o que verifica em que andar o elevador est�
         CheckCurrentLevel(levels);
     }
 
@@ -74,7 +65,6 @@ public class ElevatorMovement : MonoBehaviour
     void GoTo(Transform target, Rigidbody Rb)
     {
 
-        // Determinar� se o elevador ir� pra cima ou pra baixo
         if (target.position.y > Rb.position.y)
         {
             Rb.MovePosition(Rb.position + (Vector3.up * Time.deltaTime * elevatorSpeed));
@@ -85,7 +75,6 @@ public class ElevatorMovement : MonoBehaviour
             Rb.MovePosition(Rb.position + (-Vector3.up * Time.deltaTime * elevatorSpeed));
         }
 
-        // Verifica se o elevador est� proximo do ponto, para evitar tremidas indesejadas
         if (Vector3.Distance(target.position, Rb.position) < 0.05)
         {
             Rb.MovePosition(target.position);
