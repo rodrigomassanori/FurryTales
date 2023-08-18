e8858036b4d4f20ea2b31b36deee102b119f8a3a
TheVaxStudio <rodrigomassanori@hotmail.com>
2023-08-18 13:53:42 -0300

    all

--- Assets/Scripts/TheDeformers.cs
+++ Assets/Scripts/TheDeformers.cs
@@ -1,9 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using System.Collections;
 
 public class TheDeformers : MonoBehaviour
 {
+    NavMeshAgent nv;
 
+    public GameObject TD;
+
+    public GameObject PL;
+
+    float SpeedMovement = 3.5f;
+
+    AudioSource CreepyFox;
+
+    void Awake()
+    {
+        CreepyFox = GetComponent<AudioSource>();
+
+        nv = GetComponent<NavMeshAgent>();
+    }
+
+    void Start()
+    {
+        if (TD.gameObject.tag == "Monster")
+        {
+            CreepyFox.Play();
+            
+            transform.Translate(0, Time.deltaTime, 10, Space.World);
+        }
+    }
+
+    void Update()
+    {
+        StartCoroutine(BackToSpawn());
+    }
+
+    IEnumerator BackToSpawn()
+    {
+        yield return new WaitForSeconds(20.0f);
+
+        transform.Translate(0, Time.deltaTime, 0, Space.World);
+    }
 }
