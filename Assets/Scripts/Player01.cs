353f7984f302a26551a3374f368639cfc8daf6d4
TheVaxStudio <rodrigomassanori@hotmail.com>
2024-01-08 16:35:17 -0300

    all

---
+++ Assets/Scripts/Player01.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player01 : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    [SerializeField]
+    float MoveSpeed;
+
+    
+    void Awake()
+    {
+        
+    }
+}
