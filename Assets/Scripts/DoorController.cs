67d319b1c3053cee21c039d66a567412107b26c5
TheVaxStudio <rodrigomassanori@hotmail.com>
2024-01-17 11:54:40 -0300

    saved

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -3,9 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class DoorController : MonoBehaviour
 {
-    void OnCollisionEnter2D(Collision2D other)
+    void OnCollisionEnter2D(Collision2D Pl)
     {
-        if(other.gameObject.tag == "Player")
+        if(Pl.gameObject.tag == "Player")
         {
             SceneManager.LoadScene("Game2");
         }
