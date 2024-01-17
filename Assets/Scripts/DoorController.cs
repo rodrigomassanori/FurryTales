6b6afe27f84436fd794dc15bfb43da897e6dadce
TheVaxStudio <rodrigomassanori@hotmail.com>
2024-01-17 11:46:31 -0300

    saved

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -1,6 +1,15 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DoorController : MonoBehaviour
 {
-    
+    public GameObject Pl;
+
+    void Start()
+    {
+        if(Pl.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("Game2");
+        }
+    }
 }
