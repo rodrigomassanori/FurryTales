22a8a4aef394eb1666793936874ce7942b13644f
TheVaxStudio <rodrigomassanori@hotmail.com>
2024-01-23 09:58:19 -0300

    saved

--- Assets/Scripts/Partner.cs
+++ Assets/Scripts/Partner.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class Partner : MonoBehaviour
 {
 	float Speed = 3.0f;
+
+    Transform Player;
 	
 	Animator Anim;
     
@@ -13,12 +15,19 @@ public class Partner : MonoBehaviour
         Anim = GetComponent<Animator>();
 
         Rb = GetComponent<Rigidbody2D>();
+
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     void Update()
     {
-        Speed = Rb.velocity.magnitude;
+        if (Player != null)
+        {
+            Vector2 Direction = Player.position - transform.position;
 
-        Anim.SetFloat("Speed", Speed);
+            Direction.Normalize();
+
+            transform.Translate(Direction * Speed * Time.deltaTime);
+        }
     }
 }
