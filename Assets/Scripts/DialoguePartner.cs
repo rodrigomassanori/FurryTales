24487894fe1d5344cc092d12ba09792f1c4fcc52
TheVaxStudio <rodrigomassanori@hotmail.com>
2024-01-23 13:54:56 -0300

    saved

--- Assets/Scripts/DialoguePartner.cs
+++ Assets/Scripts/DialoguePartner.cs
@@ -1,7 +1,56 @@
+using System.Collections.Generic;
 using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class DialoguePartner : MonoBehaviour 
 {
+    public TextMeshProUGUI DialogueText;
 
+    private Queue<string> Sentences;
+
+    void Start()
+    {
+        Sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(string[] Dialogue)
+    {
+        Sentences.Clear();
+
+        foreach (string Sentence in Dialogue)
+        {
+            Sentences.Enqueue(Sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (Sentences.Count == 0)
+        {
+            EndDialogue();
+
+            return;
+        }
+
+        string Sentence = Sentences.Dequeue();
+
+        DialogueText.text = Sentence;
+    }
+
+    void EndDialogue()
+    {
+        StartCoroutine(HideDialogue());
+    }
+
+    IEnumerator HideDialogue()
+    {
+        yield return new WaitForSeconds(10.0f);
+
+        DialogueText.enabled = false;
+
+        DialogueText.gameObject.SetActive(false);
+    }
 }
