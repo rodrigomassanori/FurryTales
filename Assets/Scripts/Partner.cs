7d2f4ed2ca3d83365ccd296edb2e8faba2abbc6c
TheVaxStudio <rodrigomassanori@hotmail.com>
2024-01-23 14:25:24 -0300

    saved

--- Assets/Scripts/Partner.cs
+++ Assets/Scripts/Partner.cs
@@ -2,5 +2,21 @@ using UnityEngine;
 
 public class Partner : MonoBehaviour
 {
-    
+    public string[] Dialogue;
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                StartDialogue();
+            }
+        }
+    }
+
+    void StartDialogue()
+    {
+        FindObjectOfType<DialoguePartner>().StartDialogue(Dialogue);
+    }
 }
