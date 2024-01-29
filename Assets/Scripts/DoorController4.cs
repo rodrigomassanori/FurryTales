80351bd9ac5f75ab69a57470513b861aa49b9871
TheVaxStudio <rodrigomassanori@hotmail.com>
2024-01-29 14:30:33 -0300

    saved

---
+++ Assets/Scripts/DoorController4.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorController4 : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D Door4)
+	{
+		if (Door4.gameObject.CompareTag("Player"))
+		{
+			SceneManager.LoadScene("Game4");
+		}
+	}
+}
