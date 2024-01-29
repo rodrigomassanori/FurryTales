8040ac6f028b859b2c1a532d1971d70dee25f1ea
TheVaxStudio <rodrigomassanori@hotmail.com>
2024-01-29 11:05:09 -0300

    saved

---
+++ Assets/Scripts/ChernobylVideo.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class ChernobylVideo : MonoBehaviour 
+{
+	VideoPlayer VDPlayer;
+
+	void Awake()
+	{
+		VDPlayer = GetComponent<VideoPlayer>();
+	}
+
+	void Start()
+	{
+		if (VDPlayer != null && VDPlayer.clip != null)
+		{
+			VDPlayer.Play();
+
+			StartCoroutine(GameIntro());
+		}
+
+		else
+		{
+			print("VideoPlayer component or VideoClip not set!");
+		}
+	}
+
+	IEnumerator GameIntro()
+	{
+		yield return new WaitForSeconds(75.0f);
+
+		SceneManager.LoadScene("GameIntro");
+	}
+}
