802f918466a926fc199593a9791f6f629f1ce55c
TheVaxStudio <rodrigomassanori@hotmail.com>
2024-02-09 10:26:47 -0300

    saved

--- Assets/Scripts/TheDeformers.cs
+++ Assets/Scripts/TheDeformers.cs
@@ -1,51 +1,7 @@
 using UnityEngine;
-using UnityEngine.AI;
 using System.Collections;
 
 public class TheDeformers : MonoBehaviour
 {
-    public GameObject PL;
-
-    Transform player;
-
-    float SpeedMovement = 3.5f;
-
-    AudioSource CreepyFox;
-
-    NavMeshAgent agent;
-
-    void Awake()
-    {
-        CreepyFox = GetComponent<AudioSource>();
-
-        agent = GetComponent<NavMeshAgent>();
-
-        player = GetComponent<Transform>();
-    }
-
-    void Start()
-    {
-        if (PL.gameObject.tag == "Player")
-        {
-            //CreepyFox.Play();
-
-            agent.destination = player.position;
-            
-            transform.Translate(0, Time.deltaTime, 10 * SpeedMovement, Space.World);
-        }
-    }
-
-    void Update()
-    {
-        StartCoroutine(BackToSpawn());
-    }
-
-    IEnumerator BackToSpawn()
-    {
-        yield return new WaitForSeconds(20.0f);
-
-        agent.destination = transform.position;
-
-        transform.Translate(0, Time.deltaTime, 0, Space.World);
-    }
+    
 }
