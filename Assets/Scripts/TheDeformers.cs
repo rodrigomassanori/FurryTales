f471d030a0a91084afb609742fa55b95a87916e0
TheVaxStudio <rodrigomassanori@hotmail.com>
2024-02-09 11:11:47 -0300

    saved

--- Assets/Scripts/TheDeformers.cs
+++ Assets/Scripts/TheDeformers.cs
@@ -3,5 +3,19 @@ using System.Collections;
 
 public class TheDeformers : MonoBehaviour
 {
-    
+    float MoveSpeed = 4.0f;
+
+    Rigidbody2D Rb;
+
+    void Awake()
+    {
+        Rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Vector2 Direction = (Vector2.zero - Rb.position).normalized;
+
+        Rb.MovePosition(Rb.position + Direction * MoveSpeed * Time.deltaTime);
+    }
 }
