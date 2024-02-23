c1a7c3f64d102af82a3ed6e4085dbe798499b6cc
TheVaxStudio <rodrigomassanori@hotmail.com>
2024-02-23 13:36:29 -0300

    saved

--- Assets/Scripts/Intro2.cs
+++ Assets/Scripts/Intro2.cs
@@ -1,6 +1,29 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
 public class Intro2 : MonoBehaviour
 {
-    
+    Animator ComicAnim;
+
+    public Image Comic;
+
+    void Awake()
+    {
+        ComicAnim = GetComponent<Animator>();
+    }
+
+    void OnEnable()
+    {
+        Comic.enabled = true;
+
+        Comic.gameObject.SetActive(true);
+
+        StartCoroutine(HideComic());
+    }
+
+    IEnumerator HideComic()
+    {
+        yield return new ;
+    }
 }
