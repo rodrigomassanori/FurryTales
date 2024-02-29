8df5ef908e0b38c981a3f830e8cc0da8396c69fd
TheVaxStudio <rodrigomassanori@hotmail.com>
2024-02-29 15:51:33 -0300

    saved

--- Assets/Scripts/Tyamoon.cs
+++ Assets/Scripts/Tyamoon.cs
@@ -2,5 +2,26 @@ using UnityEngine;
 
 public class Tyamoon : MonoBehaviour 
 {
-	
+    private Rigidbody2D CharRb;
+    private Animator Anim;
+
+    Vector2 Speed;
+
+    void Awake()
+    {
+        CharRb = GetComponent<Rigidbody2D>();
+
+        Anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Vector2 speed = CharRb.velocity;
+
+        Anim.SetFloat("Vertical", speed.y);
+
+        Anim.SetFloat("Horizontal", speed.x);
+
+        Anim.SetFloat("Speed", speed.x + speed.y);
+    }
 }
