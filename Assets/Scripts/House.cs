4eba599d4dc6001018b12920e7a9c12a4118d271
TheVaxStudio <rodrigomassanori@hotmail.com>
2024-03-07 12:07:09 -0300

    saved

--- Assets/Scripts/House.cs
+++ Assets/Scripts/House.cs
@@ -1,7 +1,21 @@
 using System.Collections;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class House : MonoBehaviour 
 {
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player01"))
+        {
+            StartCoroutine(EnterHouse());
+        }
+    }
 
+    IEnumerator EnterHouse()
+    {
+        yield return new WaitForSeconds(10.0f);
+
+        SceneManager.LoadScene("Game7");
+    }
 }
