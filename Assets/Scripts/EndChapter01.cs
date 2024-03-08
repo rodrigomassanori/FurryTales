ba3235aff31ab18bc0c0742e64edea02e536bae6
TheVaxStudio <rodrigomassanori@hotmail.com>
2024-03-08 13:54:10 -0300

    saved

--- Assets/Scripts/EndChapter01.cs
+++ Assets/Scripts/EndChapter01.cs
@@ -5,5 +5,21 @@ using TMPro;
 
 public class EndChapter01 : MonoBehaviour 
 {
+    public TextMeshProUGUI EndMission;
 
+    void Start()
+    {
+        StartCoroutine(EndPart01());
+    }
+
+    IEnumerator EndPart01()
+    {
+        yield return new WaitForSeconds(5.0f);
+
+        EndMission.text = "So....we saved this world to a giant catastrophe.....\n and what will come to us?...";
+
+        yield return new WaitForSeconds(10.0f);
+
+        SceneManager.LoadScene("EndGame");
+    }
 }
