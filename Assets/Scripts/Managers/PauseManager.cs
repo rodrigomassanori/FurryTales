bd8d158a7eb3361f8e3e26f0276c35ff0fa3698e
TheVaxStudio <rodrigomassanori@hotmail.com>
2024-03-12 08:32:37 -0300

    saved

--- Assets/Scripts/Managers/PauseManager.cs
+++ Assets/Scripts/Managers/PauseManager.cs
@@ -1,7 +1,50 @@
-using System.Collections;
+using System;
 using UnityEngine;
 
 public class PauseManager : MonoBehaviour 
 {
+    public GameObject PauseBtn;
 
+    bool IsPaused;
+
+    readonly KeyCode Q = KeyCode.Q;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(Q))
+        {
+            TogglePause();
+        }
+    }
+
+    public bool TogglePause()
+    {
+        IsPaused = !IsPaused;
+
+        if (IsPaused)
+        {
+            PauseGame();
+        }
+
+        else
+        {
+            ResumeGame();
+        }
+        
+        return IsPaused;
+    }
+
+    void PauseGame()
+    {
+        Time.timeScale = 0.0f;
+
+        PauseBtn.SetActive(true);
+    }
+
+    void ResumeGame()
+    {
+        Time.timeScale = 1.0f;
+
+        PauseBtn.SetActive(false);
+    }
 }
