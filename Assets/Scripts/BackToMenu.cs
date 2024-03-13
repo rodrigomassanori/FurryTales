7d5890cbc99dec034c1d81129d2b2fd5ffaeba53
TheVaxStudio <rodrigomassanori@hotmail.com>
2024-03-13 08:52:54 -0300

    saved

--- Assets/Scripts/BackToMenu.cs
+++ Assets/Scripts/BackToMenu.cs
@@ -1,6 +1,18 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BackToMenu : MonoBehaviour
 {
-    
+    void Start()
+    {
+        StartCoroutine(Menu());
+    }
+
+    IEnumerator Menu()
+    {
+        yield return new WaitForSeconds(4.0f);
+
+        SceneManager.LoadScene("MainMenu");
+    }
 }
